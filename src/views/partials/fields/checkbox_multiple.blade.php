7e09bf98600dcc27f568a5e2381103be880500f7
Dave Blencowe <dblencowe@gmail.com>
2018-11-17 11:36:08 +0000

    Changes the multiple checkbox so it can re-populated with $values

--- src/views/partials/fields/checkbox_multiple.blade.php
+++ src/views/partials/fields/checkbox_multiple.blade.php
@@ -2,7 +2,7 @@
 @foreach($options as $value => $displayName)
 <div class="checkbox">
     <label>
-        <input type="{{ $type or 'checkbox' }}" name="{{ $name }}[]" {{ isset($checked) && $checked ? 'CHECKED ' : '' }} value="{{ $value or '1' }}" {{ isset($disabled) && $disabled ? 'DISABLED' : '' }} class="{{ $classes or '' }}"> {{ $displayName }}
+        <input type="{{ $type or 'checkbox' }}" name="{{ $name }}[]" {{ isset($values) && in_array($value, $values) ? 'CHECKED ' : '' }} value="{{ $value or '1' }}" {{ isset($disabled) && $disabled ? 'DISABLED' : '' }} class="{{ $classes or '' }}"> {{ $displayName }}
     </label>
 </div>
 @endforeach
